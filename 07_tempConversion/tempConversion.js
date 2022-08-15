ca9836a12c8ddae6a2b31122fb06d6997926bef1
Harevald <jakubkrasuski3@gmail.com>
2022-08-15 21:21:06 +0200

    Exercise 6 completed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,24 @@
-const ftoc = function() {
-
+const ftoc = function(temperature) {
+    temperature = (temperature-32)*(5/9);
+    if(temperature==Math.round(temperature)){
+      return temperature;
+    }
+    else{
+      temperature = parseFloat(temperature.toFixed(1));
+      return temperature;
+    }
+    
 };
 
-const ctof = function() {
-
+const ctof = function(temperature) {
+  temperature = temperature * (9/5) + 32;
+  if(temperature==Math.round(temperature)){
+    return temperature;
+  }
+  else{
+    temperature = parseFloat(temperature.toFixed(1));
+    return temperature;
+  }
 };
 
 // Do not edit below this line
