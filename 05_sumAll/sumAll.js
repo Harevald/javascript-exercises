1be93959a49b59489a250e91d654220a117306eb
Harevald <jakubkrasuski3@gmail.com>
2022-08-15 20:22:24 +0200

    Exercise 5 completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    let sum = 0;
+    if(start < 0 || end < 0){ //Jeśli start albo end są mniejsze od 0 to wyrzuć ERROR
+        return "ERROR"; 
+    }
+    else if(typeof(start)!="number" || typeof(end)!="number" ){ //Jeśli start albo end nie są numerami to wyrzuć ERROR
+        return "ERROR";
+    }
+    else{
+        if(start<end){ //Jeśli start jest mniejszy od end, to dodawaj po kolei liczby z przedziału start-end
+            for(let i=start; i<=end; i++){
+                sum+=i;
+            }
+        }
+        else{ //Jeśli end jest mniejsze od start, to dodawaj liczby z przedziału end-start
+            for(let i=end; i<=start; i++){
+                sum+=i;
+            }
+        }
+        
+        return sum;
+    }
+    
 };
 
 // Do not edit below this line
