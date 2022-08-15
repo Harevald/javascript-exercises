d46552274616b5e3c6a30be3f10d02646633c416
Harevald <jakubkrasuski3@gmail.com>
2022-08-15 17:15:08 +0200

    Exercise 02_repeatString completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(text, num) {
+    if(num<0){
+        return "ERROR";
+    }
+    else{
+        let temp = "";
+        for(let i = 0; i<num; i++){
+            temp = temp + text;
+        }
+        return temp;
+    }
+    
 };
 
 // Do not edit below this line
