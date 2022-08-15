4c8cda499b9f78e00d121f4b8005017324d736dd
Harevald <jakubkrasuski3@gmail.com>
2022-08-15 17:34:17 +0200

    Exercise 03_reverseString completed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    let textReverse;
+    textReverse = text.split("").reverse().join(""); //Podziel string na tablicę substringów, odwróć kolejność elementów w tej tablicy i złącz substringi w całość
+                                                    //Np. dla "hello" -> 1) robimy "h", "e", "l", "l", "o" 2) odwracamy kolejność 3) łączymy litery do "olleh" zamiast liter osobno
+    return textReverse;
 };
 
 // Do not edit below this line
