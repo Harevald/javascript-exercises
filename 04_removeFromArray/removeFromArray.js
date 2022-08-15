5496c447a981fe1daf4bcd6854e9ad9df93494ef
Harevald <jakubkrasuski3@gmail.com>
2022-08-15 18:24:38 +0200

    Exercise 04_removeFromArray completed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) { //...args oznacza dowolną ilość argumentów, przechowaną w tablicy args[]
+    let index;
+    for(let i=0; i<args.length; i++){ //Wykonuj pętle tak wiele razy jak długa jest lista argumentów w args[]
+        if(array.includes(args[i])){ //Wykonaj usunięcie tylko wtedy, jeśli dany element istnieje w tablicy array[]
+            index = array.indexOf(args[i]); //Pobierz indeks argumentu, który będziemy chcieli usunąć
+            array.splice(index, 1); //Usuń z tablicy array[] element o indeksie index
+        }
+        
+    } 
+    
+    return array;
 };
 
 // Do not edit below this line
