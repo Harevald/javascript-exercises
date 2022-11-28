9e635608668206681db26fd8b8e056a924f8c09d
Harevald <jakubkrasuski3@gmail.com>
2022-11-28 18:03:18 +0100

    All tests passed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,21 @@
-const fibonacci = function() {
-
+const fibonacci = function (n) {
+    n = Number(n);
+    if(n>=0)
+  {
+     let sum = Array(n);
+  sum[0] = 1;
+  sum[1] = 1;
+  if (n === 0) return 0;
+  else if (n === 1) return 1;
+  else {
+    for (let i = 2; i < n; i++) {
+      sum[i] = sum[i - 1] + sum[i - 2];
+    }
+    return sum[n-1];
+  }
+  }
+  else return ("OOPS");
+   
 };
-
 // Do not edit below this line
 module.exports = fibonacci;
