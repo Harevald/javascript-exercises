bcab7970c30eaf782227f4def576a67789876e30
Harevald <jakubkrasuski3@gmail.com>
2022-11-29 19:30:23 +0100

    All tests passed

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,27 +1,43 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+    let sumA;
+    sumA = a.reduce((partialSum, a) => partialSum + a, 0);  
+    return sumA;
 };
 
-const multiply = function() {
-
+const multiply = function(a) {
+  let product = a.reduce((partialProduct, a) => partialProduct * a, 1);
+  return product;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	let result=1;
+  for(let i=0; i<b; i++)
+    result = result * a;
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let result=1;
+  if(a == 0)
+    result = 1;
+  else{
+    for(let i=1; i<=a; i++)
+    {
+      result = result * i;
+      console.log(result);
+    }
+      
+  }
+  return result;
 };
-
 // Do not edit below this line
 module.exports = {
   add,
